5add890fbd5ce7f79e7f9d528a256362f3029b86
ChrTor <38206602+ChrTor@users.noreply.github.com>
2020-01-22 11:55:19 +0100

    Initial

---
+++ main.php
@@ -0,0 +1,31 @@
+<!DOCTTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>"John $Wicktim"</title>
+
+  </head>
+  <body>
+
+// Story of John Wick
+    include "class_Human.inc";
+
+            echo "<hr>";
+
+        $Wick = new Human('John', 'Wick', 'Fist', 'Punch');
+        $Wicktim = new Human('Burt', 'Wicktim', 'Fist', 'Punch');
+
+
+
+
+
+        echo "<p>$data.Name is $Age, $Name enjoys he's $drink $color and with a lot of $info[2].</p>";
+        echo "<p> This is me mucking about with php </p>";
+     ?>
+
+
+
+
+  </body>
+
+</html>
